8cd838019566283ddfdc99fc289b4bb660c84095
kurthivas <kurthivas.qa@outloo.com>
2019-06-01 16:48:13 +0530

    kruthivas updated

---
+++ src/test/java/Academy/rona.java
@@ -0,0 +1,24 @@
+package Academy;
+
+import org.testng.annotations.Test;
+
+import resources.base;
+
+public class rona extends base {
+	@Test
+	public void rona() {
+		System.out.println("kruthivas");
+		System.out.println("kruthivas");
+		System.out.println("kruthivas");
+		System.out.println("kruthivas");
+		System.out.println("kruthivas");
+		System.out.println("kruthivas");
+		System.out.println("kruthivas");
+		System.out.println("kruthivas");
+		System.out.println("kruthivas");
+		System.out.println("kruthivas");
+		System.out.println("kruthivas");
+		System.out.println("kruthivas");
+	}
+
+}
