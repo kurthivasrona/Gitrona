9b67703ade4f48baefada72cc26d2d36413aed04
kurthivas <kurthivas.qa@outloo.com>
2019-06-01 15:59:39 +0530

    This is  my 3st commit

---
+++ src/main/java/pageObjects/kruthivas.java
@@ -0,0 +1,19 @@
+package pageObjects;
+
+import org.testng.annotations.Test;
+
+import resources.base;
+
+public class kruthivas  extends base{
+	@Test
+	public void rona() {
+		System.out.println("kruthivasrona");
+		System.out.println("kruthivasrona");
+		System.out.println("kruthivasrona");
+		System.out.println("kruthivasrona");
+		System.out.println("kruthivasrona");
+		System.out.println("kruthivasrona");
+		
+	}
+
+}
